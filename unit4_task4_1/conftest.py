8497f571635d0428e80bf73ad44920a2864b16ff
Krylova Ksenia <xenya.p@gmail.com>
2022-04-24 18:13:11 +1000

    добавлены файлы для выполнения 4го модуля

---
+++ unit4_task4_1/conftest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default= 'en',
+                     help="Choose browser language, please")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
