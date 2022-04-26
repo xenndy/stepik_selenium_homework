edeabafa549314b669c541bf47feed6d07222b82
Krylova Ksenia <xenya.p@gmail.com>
2022-04-26 14:39:02 +1000

    Реализация проверки login_page

---
+++ unit4_task4_1/pages/locators.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+        
--- unit4_task4_1/test_main_page.py
+++ unit4_task4_1/test_main_page.py
@@ -5,17 +5,25 @@
 from selenium import webdriver
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+LINK = "http://selenium1py.pythonanywhere.com/"
+
+# def test_guest_can_go_to_login_page(browser):
+#     page = MainPage(browser, LINK)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+#     page.open()                      # открываем страницу
+#     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+# def test_guest_should_see_login_link(browser):
+#     # link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, LINK)
+#     page.open()
+#     page.should_be_login_link()
 
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+def test_guest_should_see_login_n_register_form(browser):
+    login_page_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, login_page_link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_page()
