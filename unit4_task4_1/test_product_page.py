0f3206f4840834bee1568279f64860b0eed72b5d
Krylova Ksenia <xenya.p@gmail.com>
2024-05-20 13:22:47 +1000

    добавлены xfail к падающим тестам

--- unit4_task4_1/test_product_page.py
+++ unit4_task4_1/test_product_page.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from .pages.product_page import ProductPage
-# import pytest
+import pytest
 
 QUIZ_LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -20,14 +20,14 @@ LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
-def _test_guest_can_add_product_to_basket(browser):
+def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, QUIZ_LINK)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_messages()
 
-
+@pytest.mark.xfail(reason="тестирование отрицательной проверки")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, LINK)
     page.open()
@@ -39,15 +39,11 @@ def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, LINK)
     page.open()
     page.should_not_be_success_message()
-# Открываем страницу товара
-# Проверяем, что нет сообщения об успехе с помощью is_not_element_present
 
+@pytest.mark.xfail(reason="тестирование отрицательной проверки")
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, LINK)
     page.open()
     page.add_to_basket()
     page.should_be_disappeared()
 
-# Открываем страницу товара
-# Добавляем товар в корзину
-# Проверяем, что нет сообщения об успехе с помощью is_disappeared
